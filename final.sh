20f98fd5a9e6a0acf744cf741daf4cf5f4ea84b3
Alahi Majnur Osama <123330241+AlahiMajnurOsama@users.noreply.github.com>
2023-04-03 00:52:32 -0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -1,1 +1,7 @@
+ngrok_cmd="sudo ngrok tcp 22 --log \".ngrok.log\" &> /dev/null &"
 
+# Start a new screen session and execute the Ngrok command in it
+screen -S ngrok -d -m bash -c "${ngrok_cmd}"
+
+# Print confirmation message
+echo "Ngrok tunnel started in a new screen session."
